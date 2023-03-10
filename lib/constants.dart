231cade9a0519386dcaacabcccad581ce9f25c38
RodLA <rodrigoloayzab@gmail.com>
2023-03-10 18:16:56 -0500

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+// ignore: constant_identifier_names
+const String google_api_key = "AIzaSyAd4rEAQqf58fCJGABqW99teDP9BcuyN08";
+const Color primaryColor = Color(0xFF7B61FF);
+const double defaultPadding = 16.0;
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_mao/map_sample.dart';
+import 'const/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: const ColorScheme(
+            brightness: Brightness.dark,
+            primary: ColorsMyApp.secondarycolor,
+            onPrimary: ColorsMyApp.optionalcolor,
+            secondary: ColorsMyApp.primarycolor,
+            onSecondary: ColorsMyApp.tertiarycolor,
+            error: ColorsMyApp.errorcolor,
+            onError: ColorsMyApp.onErrorcolor,
+            background: ColorsMyApp.backgroundcolor,
+            onBackground: ColorsMyApp.onbackgroundcolor,
+            surface: ColorsMyApp.surfacecolor,
+            onSurface: ColorsMyApp.onsurfacecolor),
+      ),
+      home: const MapSample(),
+    );
+  }
+}
